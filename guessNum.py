e52ccf7cf1d6719ead931231a3290e08b9225ae7
Luis Gonzalez <162064187+LuisJG8@users.noreply.github.com>
2024-07-15 21:47:38 -0400

    Add files via upload

---
+++ guessNum.py
@@ -0,0 +1,8 @@
+from logo import gtn
+import random
+
+random_number = random.randint(0, 100)
+
+print(gtn)
+print("Welcome to the Number Guessing Game!\nI am thinking of a number between 1 and 100")
+print(f"The correct answer is {random_number}")
